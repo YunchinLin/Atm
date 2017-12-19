61844f8e725497567b034ddd21bbab4cba18ed17
Bess Lin <bess861231@gmail.com>
2017-12-19 11:41:02 +0800

    Spinner, ages resources array Spinner

--- app/src/main/java/com/bess/atm/UserInfoActivity.java
+++ app/src/main/java/com/bess/atm/UserInfoActivity.java
@@ -2,17 +2,34 @@ package com.bess.atm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AbsSpinner;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class UserInfoActivity extends AppCompatActivity {
     private EditText edNickname;
     private EditText edPhone;
+    private static final String TAG = UserInfoActivity.class.getSimpleName();
+    private Spinner ages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        ages = (Spinner) findViewById(R.id.ages);
+//        ArrayList<String> data = new ArrayList<>();
+//        for (int i = 15; i<=40; i++){
+//            data.add(i+"");
+//        }
+//        String[] data = getResources().getStringArray(R.array.ages);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.aaa, android.R.layout.simple_list_item_1);
+        ages.setAdapter(adapter);
         edNickname = (EditText) findViewById(R.id.nickname);
         edPhone = (EditText) findViewById(R.id.phone);
         String nickname = getSharedPreferences("atm", MODE_PRIVATE)
@@ -23,7 +40,9 @@ public class UserInfoActivity extends AppCompatActivity {
         edPhone.setText(phone);
     }
 
-    public void back(View view){
+    public void ok(View view){
+        Log.d(TAG, "ok:" + ages.getSelectedItem().toString());
+        int age = Integer.parseInt(ages.getSelectedItem().toString());
         EditText edNickname = (EditText) findViewById(R.id.nickname);
         EditText edPhone = (EditText) findViewById(R.id.phone);
         String nickname = edNickname.getText().toString();
